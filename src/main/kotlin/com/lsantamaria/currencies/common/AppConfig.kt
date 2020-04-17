a174c24bbc5cb5109cfb914e9d8cce8ed167beaf
Lluis Santamaria <lluis.santamariasola@mastercard.com>
2020-04-17 12:26:37 +0100

    try spring webflux config for deployment

--- src/main/kotlin/com/lsantamaria/currencies/common/AppConfig.kt
+++ src/main/kotlin/com/lsantamaria/currencies/common/AppConfig.kt
@@ -2,15 +2,22 @@ package com.lsantamaria.currencies.common
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.reactive.CorsWebFilter
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurerComposite
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
 
 
 
 @Configuration
-class AppConfig{
+class WebFluxConfig {
     @Bean
-    fun corsFilter(): CorsWebFilter {
-        return CorsWebFilter { exchange -> CorsConfiguration().applyPermitDefaultValues() }
+    fun corsConfigurer(): WebFluxConfigurer {
+        return object : WebFluxConfigurerComposite() {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry!!.addMapping("/**").allowedOrigins("*")
+                        .allowedMethods("*")
+            }
+        }
     }
 }
