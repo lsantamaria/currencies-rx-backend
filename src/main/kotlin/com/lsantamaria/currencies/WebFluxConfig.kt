318025eae61d11f187d8d08be32890451d8b4552
Lluis Santamaria <lluis.santamariasola@mastercard.com>
2020-04-17 12:33:18 +0100

    adding CORS configuration in WebFluxConfigurer

--- src/main/kotlin/com/lsantamaria/currencies/WebFluxConfig.kt
+++ src/main/kotlin/com/lsantamaria/currencies/WebFluxConfig.kt
@@ -3,7 +3,16 @@ package com.lsantamaria.currencies
 import org.springframework.web.reactive.config.EnableWebFlux
 import org.springframework.web.reactive.config.WebFluxConfigurer
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
 
 @Configuration
 @EnableWebFlux
-class WebFluxConfig : WebFluxConfigurer
+class WebFluxConfig : WebFluxConfigurer{
+    override fun addCorsMappings(registry: CorsRegistry)
+    {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // any host or put domain(s) here
+                .allowedMethods("GET, POST", "PUT", "DELETE", "OPTIONS") // put the http verbs you want allow
+                .allowedHeaders("x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN") // put the http headers you want allow
+    }
+}
